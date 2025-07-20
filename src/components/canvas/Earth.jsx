c1b70f80f46796633e2bd1a50548494e591a26ff
Piyumi-Rathnayake <piumi.r@parallaxtec.com>
2025-07-20 18:15:28 +0530

    initial commit

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,6 +1,6 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
 
 import CanvasLoader from "../Loader";
 
@@ -8,7 +8,7 @@ const Earth = () => {
   const earth = useGLTF("./planet/scene.gltf");
 
   return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+    <primitive object={earth.scene} scale={1.8} position-y={0} rotation-y={0} />
   );
 };
 
@@ -16,7 +16,7 @@ const EarthCanvas = () => {
   return (
     <Canvas
       shadows
-      frameloop='demand'
+      frameloop="demand"
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
       camera={{
